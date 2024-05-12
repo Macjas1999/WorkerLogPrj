66ad161d7771d6dfd81b2a9ad301e142d9d8b6d4
Maciej <MaciejJ1999@interia.pl>
2024-05-12 14:43:04 +0200

    Creation of emploees change

--- resources/views/create_emploee.blade.php
+++ resources/views/create_emploee.blade.php
@@ -11,6 +11,27 @@
         @csrf
         <label for="name">Name:</label>
         <input type="text" id="name" name="name">
+
+        <label for="surname">Surame:</label>
+        <input type="text" id="surname" name="surname">
+
+        <label for="email">Email adress:</label>
+        <input type="text" id="email" name="email">
+
+        <label for="contact_number">Phone number:</label>
+        <input type="text" id="contact_number" name="contact_number">
+
+        <label for="position">Position in company:</label>
+        <input type="text" id="position" name="position">
+
+        <label for="sallary_per_hour">Sallary per hour:</label>
+        <input type="text" id="sallary_per_hour" name="sallary_per_hour">
+
+        <label for="date_of_birth">Date of birth:</label>
+        <input type="text" id="date_of_birth" name="date_of_birth">
+
+        <label for="date_of_employment">Date of employment:</label>
+        <input type="text" id="date_of_employment" name="date_of_employment">
         <!-- Add other input fields for employee details -->
         <button type="submit">Submit</button>
     </form>
